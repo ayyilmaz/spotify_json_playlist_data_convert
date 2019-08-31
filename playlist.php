23c3e4b8e34893f0c48c5b8397780d082d5ae3f2
Ali Yasir Yılmaz <aliyasiryilmaz@gmail.com>
2019-08-31 19:45:04 +0300

    Update playlist.php

--- playlist.php
+++ playlist.php
@@ -6,18 +6,15 @@ $json = json_decode($fgc,true);
 
 echo '<div style="margin: auto;">';
 foreach($json['playlists'] as $mydata)
-
 {
 	echo '<h3>'.$mydata['name'] .'</h3>';
 	foreach($mydata['items'] as $songs)
-	{
-		
+	{	
 		echo '<div style="padding: 2px 5px; background:lightgrey; margin-bottom: 4px; margin-right:1px; width: 205px; min-height: 160px; display: inline-table;"';
 		echo '<h5>Track Name: '.$songs['track']['trackName'] .'</h5>';
 		echo '<h5>Artist Name: '.$songs['track']['artistName'].'</h5>';
-    echo '<h5>Album Name: '.$songs['track']['albumName'].'</h5>';
-		echo '</div>';
-    
+    		echo '<h5>Album Name: '.$songs['track']['albumName'].'</h5>';
+		echo '</div>';    
 	}
 }  
 echo '</div>';
